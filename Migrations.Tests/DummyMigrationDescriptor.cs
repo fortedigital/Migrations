43ad7f1bce53091769af21a5637ef2f132e6ad1b
Maciej Grzyb <maciej.grzyb@fortedigital.no>
2018-03-18 20:38:06 +0100

    Added MigrationDescriptorTests

---
+++ Migrations.Tests/DummyMigrationDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Forte.Migrations.Tests
+{
+    class DummyMigrationDescriptor : MigrationDescriptor
+    {
+        public DummyMigrationDescriptor(string id, string name, int? sequenceNo = null, IEnumerable<string> dependencies = null) : base(id, name, sequenceNo, dependencies)
+        {
+        }
+
+        public override IMigration CreateMigration(IActivator<IMigration> activator)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Migrations.Tests/MigrationDescriptorTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Forte.Migrations.Tests
+{
+    [TestFixture]
+    public class MigrationDescriptorTests
+    {
+        [Test]
+        public void CompareBySequenceNo()
+        {
+            var m1 = new DummyMigrationDescriptor("1", "1", 1);
+            var m2 = new DummyMigrationDescriptor("2", "2", 2);
+
+            Assert.That(m1.CompareTo(m2), Is.LessThan(0));
+            Assert.That(m2.CompareTo(m1), Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void CompareByDependency()
+        {
+            var m1 = new DummyMigrationDescriptor("1", "1");
+            var m2 = new DummyMigrationDescriptor("2", "2", null, new [] { "1" });
+
+            Assert.That(m1.CompareTo(m2), Is.LessThan(0));
+            Assert.That(m2.CompareTo(m1), Is.GreaterThan(0));
+        }        
+    }
+}
